3b42716a5db3f3c29fd9922df3b139a2900bf191
Rusmir Alexandru <alexandru.rusmir02@e-uvt.ro>
2022-04-03 20:40:32 +0300

    commit1

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+
+
+@NgModule({
+  declarations: [
+    AdminDashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    ProductsListComponent,
+    UsersListComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
